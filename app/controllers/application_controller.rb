4592db547ee1e59c6b40aa20821561c3bcb3b149
Sebastien Saunier <seb@saunier.me>
2014-12-04 08:25:01 +0100

    Rescue gently from Pundit::NotAuthorizedError

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,13 @@ class ApplicationController < ActionController::Base
 
   after_action :verify_authorized, :except => :index, unless: :devise_controller?
   after_action :verify_policy_scoped, :only => :index, unless: :devise_controller?
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(root_path)
+  end
 end
